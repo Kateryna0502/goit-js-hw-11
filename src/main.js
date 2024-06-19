985d8c68c86a2409430e62527c5ddc25bbd85e0f
Kateryna <mweoliss74@gmail.com>
2024-06-19 21:55:21 +0300

    w

--- src/main.js
+++ src/main.js
@@ -45,39 +45,3 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
 
-// import { getImage } from './js/pixabay-api.js';
-// import { marcupImage, showLoader, hideLoader } from './js/render-functions.js';
-// import SimpleLightbox from 'simplelightbox';
-// import 'simplelightbox/dist/simple-lightbox.min.css';
-// import 'simplelightbox/dist/simple-lightbox.min.js';
-// import iziToast from 'izitoast';
-// import 'izitoast/dist/css/iziToast.min.css';
-
-// export const refs = {
-//   formSearch: document.querySelector('#search'),
-//   inputImgSearch: document.querySelector('.input-img-search'),
-//   imgGallery: document.querySelector('.gallery'),
-//   loader: document.querySelector('.loader'),
-// };
-
-// refs.formSearch.addEventListener('submit', event => {
-//   event.preventDefault();
-//   const imgKeyWord = refs.inputImgSearch.value.trim();
-//   console.log(imgKeyWord);
-//   if (imgKeyWord === '') {
-//     iziToast.error({
-//       title: 'Error',
-//       message:
-//         'Sorry, there are no images matching your search query. Please try again!',
-//     });
-//     return;
-//     refs.imgGallery.innerHTML = '';
-//   } else {
-//     showLoader();
-//     getImage(imgKeyWord)
-//       .then(data => marcupImage(data))
-//       .catch(err => {});
-//     hideLoader();
-//   }
-//   refs.formSearch.reset();
-// });
