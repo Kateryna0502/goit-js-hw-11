0ab3956735bb50a11879fbea7ab9fc085ce521fb
Kateryna <mweoliss74@gmail.com>
2024-06-20 14:48:24 +0300

    n

--- src/main.js
+++ src/main.js
@@ -1,29 +1,32 @@
+// import iziToast from 'izitoast';
+// import 'izitoast/dist/css/iziToast.min.css';
+// import { searchImages } from './js/pixabay-api';
+// import { displayImages } from './js/render-functions';
+
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 import { searchImages } from './js/pixabay-api';
 import { displayImages } from './js/render-functions';
+const searchForm = document.getElementById('search-form');
+const searchInput = document.getElementById('search-input');
+const loader = document.getElementById('loader');
 
-document.addEventListener("DOMContentLoaded", () => {
-    const searchForm = document.getElementById('search-form');
-    const searchInput = document.getElementById('search-input');
-    const loader = document.getElementById('loader');
-
-    searchForm.addEventListener('submit', event => {
-        event.preventDefault();
-        const query = searchInput.value.trim();
-
-        if (query === '') {
-            iziToast.error({
-                title: 'Error',
-                message: 'Search query cannot be empty!'
-            });
-            return;
-        }
-    })
-
+searchForm.addEventListener('submit', event => {
+    event.preventDefault();
     loader.style.display = 'block';
-try {
-    searchImages(query).then((data) => 
+    const query = searchInput.value.trim();
+    if (query === '') {
+        iziToast.error({
+            title: 'Error',
+            message: 'Search query cannot be empty!'
+        });
+        return;
+    }
+})
+// loader.style.display = 'block';
+const images = searchImages(query);
+images.then((data) => {
+    
     if (data.hits.length === 0) {
         iziToast.error({
             title: 'Sorry',
@@ -31,16 +34,83 @@ try {
         });
     } else {
         displayImages(images);
-    }).catch((error) => {
-        iziToast.error({
-            title: 'Error',
-            message: 'Failed to fetch images. Please try again later.'
-        })
-    }).finally(() => {
-        loader.style.display = 'none';
-        refs.searchForm.reset();
+    }
+}).catch((error) => {
+    iziToast.error({
+        title: 'Error',
+        message: 'Failed to fetch images. Please try again later.'
     })
-)
+}).finally(() => {
+    loader.style.display = 'none';
+    refs.searchForm.reset();
+})
+
+
+
+
+//         const images = searchImages(query);
+//         if (images.length === 0) {
+//             images.then(data => displayImages(data.hits));
+//             iziToast.error({
+//                 title: 'Sorry',
+//                 message: 'There are no images matching your search query. Please try again!'
+//             });
+//         } else {
+//             displayImages(images);
+//         }
+//     } images.catch(err => {
+//         iziToast.error({
+//             title: 'Error',
+//             message: 'Failed to fetch images. Please try again later.'
+//         });
+//     };
+//         images.finally {
+//             loader.style.display = 'none';
+//         }
+//     });
+// });
+
+
+
+
+// document.addEventListener("DOMContentLoaded", () => {
+//     const searchForm = document.getElementById('search-form');
+//     const searchInput = document.getElementById('search-input');
+//     const loader = document.getElementById('loader');
+
+//     searchForm.addEventListener('submit', event => {
+//         event.preventDefault();
+//         const query = searchInput.value.trim();
+
+//         if (query === '') {
+//             iziToast.error({
+//                 title: 'Error',
+//                 message: 'Search query cannot be empty!'
+//             });
+//             return;
+//         }
+//     })
+
+//     loader.style.display = 'block';
+// try {
+//     searchImages(query).then((data) => 
+//     if (data.hits.length === 0) {
+//         iziToast.error({
+//             title: 'Sorry',
+//             message: 'There are no images matching your search query. Please try again!'
+//         });
+//     } else {
+//         displayImages(images);
+//     }).catch((error) => {
+//         iziToast.error({
+//             title: 'Error',
+//             message: 'Failed to fetch images. Please try again later.'
+//         })
+//     }).finally(() => {
+//         loader.style.display = 'none';
+//         refs.searchForm.reset();
+//     })
+// )
 
     
     
